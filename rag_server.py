19077599afe0d12b749953ee439e7957e72d7a90
akuazuk <akuazuk@gmail.com>
2026-08-09 13:23:58 +0300

    chore: bump BUILD_VERSION after rebase onto main

--- rag_server.py
+++ rag_server.py
@@ -8460,7 +8460,7 @@ def _icd_ru_entries_count() -> int:
 
 
 # Версия сборки: меняйте при значимых изменениях, чтобы по сайту/ответам видеть, новый ли код развёрнут.
-BUILD_VERSION = "2026-08-09-100801Z-methodist-cabinet-style"
+BUILD_VERSION = "2026-08-09-102358Z-methodist-cabinet-style"
 
 
 def _app_version() -> str:
